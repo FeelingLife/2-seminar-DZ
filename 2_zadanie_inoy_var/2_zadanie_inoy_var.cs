1868a3e47e00bf7f67b1e65bb29d0c7255608718
Nikita <crazefire@mail.ru>
2023-04-12 22:36:33 +0300

    обавил еще 1 варинат решения

---
+++ 2_zadanie_inoy_var/2_zadanie_inoy_var.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число: ");
+string text_number = Console.ReadLine();
+int number = Convert.ToInt32(text_number);
+int result = number;
+int count = 0;
+
+number = Math.Abs(number);
+if (number < 100) 
+{
+    Console.Write(number);
+    Console.Write(" - в числе нет 3-ей цифры");
+}
+else
+{
+    while(number > 999)
+    {
+        number = number / 10;
+    }
+    Console.Write("Третья цифра в этом числе: ");
+    Console.WriteLine(number % 10);
+}
